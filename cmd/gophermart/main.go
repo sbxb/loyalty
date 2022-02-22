8a3a2076c3f2178531206e6b2ad16c7b765aa6c8
sbxb <sbxb@example.com>
2022-02-22 18:01:27 +0300

    Add server (without config so far)

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"os/signal"
+	"syscall"
+
 	"github.com/sbxb/loyalty/api"
 	"github.com/sbxb/loyalty/internal/logger"
 )
@@ -9,5 +13,23 @@ func main() {
 	logger.SetLevel("DEBUG")
 
 	router := api.NewRouter()
-	_ = router
+	server, _ := api.NewHTTPServer("localhost:8888", router)
+	defer server.Close()
+
+	ctx, stop := signal.NotifyContext(
+		context.Background(), syscall.SIGTERM, syscall.SIGINT,
+	)
+	defer stop()
+
+	go func() {
+		err := server.Start(ctx)
+		if err != nil {
+			// Server either failed to start or exited unexpectedly
+			// Cancel the context in order to let main() finish its work
+			stop()
+		}
+	}()
+
+	<-ctx.Done()
+	server.Close()
 }
