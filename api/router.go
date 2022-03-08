da8e23909101bb166b9a7b2c22af3c1128606f31
sbxb <sbxb@example.com>
2022-03-08 17:55:16 +0300

    Update routes

--- api/router.go
+++ api/router.go
@@ -7,15 +7,24 @@ import (
 	"github.com/sbxb/loyalty/api/handlers"
 	"github.com/sbxb/loyalty/config"
 	"github.com/sbxb/loyalty/internal/logger"
+	"github.com/sbxb/loyalty/storage"
 )
 
-func NewRouter( /*store storage.Storage, */ cfg config.Config) http.Handler {
+func NewRouter(store storage.Storage, cfg config.Config) http.Handler {
 	router := chi.NewRouter()
 	logger.Info("Router created")
-	urlHandler := handlers.NewURLHandler( /*store, */ cfg)
+
+	urlHandler := handlers.NewURLHandler(store, cfg)
 
 	router.Post("/api/user/register", urlHandler.UserRegister)
 	router.Post("/api/user/login", urlHandler.UserLogin)
+
+	router.Post("/api/user/orders", urlHandler.UserPostOrder)
+	router.Get("/api/user/orders", urlHandler.UserGetOrders)
+
+	router.Get("/api/user/balance", urlHandler.UserGetBalance)
+	router.Post("/api/user/balance/withdraw", urlHandler.UserBalanceWithdraw)
+	router.Get("/api/user/balance/withdrawals", urlHandler.UserGetWithdrawals)
 
 	return router
 }
