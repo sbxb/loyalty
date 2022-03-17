c996986b171e13e1c8a7bac694f4c9787487c057
sbxb <sbxb@example.com>
2022-03-17 22:28:30 +0300

    Complete handlers (not tested fully)

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -2,11 +2,14 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"time"
 
 	"github.com/sbxb/loyalty/config"
 	"github.com/sbxb/loyalty/internal/logger"
 	"github.com/sbxb/loyalty/models"
+	"github.com/sbxb/loyalty/services/accrual"
 	"github.com/sbxb/loyalty/services/auth"
 	"github.com/sbxb/loyalty/services/order"
 	"github.com/sbxb/loyalty/storage"
@@ -14,18 +17,20 @@ import (
 
 // URLHandler defines a container for handlers and their dependencies
 type URLHandler struct {
-	store  storage.Storage
-	config config.Config
-	auth   *auth.AuthService
-	ord    *order.OrderService
+	store   storage.Storage
+	config  config.Config
+	auth    *auth.AuthService
+	ord     *order.OrderService
+	accrual *accrual.SimpleAccrualService
 }
 
 func NewURLHandler(st storage.Storage, cfg config.Config) URLHandler {
 	return URLHandler{
-		store:  st,
-		config: cfg,
-		auth:   auth.NewAuthService(st),
-		ord:    order.NewOrderService(st),
+		store:   st,
+		config:  cfg,
+		auth:    auth.NewAuthService(st),
+		ord:     order.NewOrderService(st),
+		accrual: accrual.NewSimpleAccrualService(st),
 	}
 }
 
@@ -59,7 +64,7 @@ func (uh URLHandler) UserRegister(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// send http.StatusOK by default
+	// http.StatusOK sent implicitly
 }
 
 // UserLogin process POST /api/user/login request
@@ -83,7 +88,7 @@ func (uh URLHandler) UserLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// send http.StatusOK by default
+	// http.StatusOK sent implicitly
 }
 
 // UserPostOrder process POST /api/user/orders request
@@ -99,11 +104,19 @@ func (uh URLHandler) UserPostOrder(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, orderRegErr.Error(), orderRegErr.Code)
 		return
 	}
+	go func() {
+		retry := uh.accrual.DoAccrualStuff(order.Number)
+		if retry {
+			time.Sleep(500 * time.Microsecond)
+			_ = uh.accrual.DoAccrualStuff(order.Number)
+		}
+	}()
 	w.WriteHeader(http.StatusAccepted)
 }
 
 // UserGetOrders process GET /api/user/orders request
 func (uh URLHandler) UserGetOrders(w http.ResponseWriter, r *http.Request) {
+	logger.Info("UserGetOrders hit by GET /api/user/orders")
 	userID := auth.GetUserID(r.Context())
 	orderList, orderRegErr := uh.ord.ListOrders(r.Context(), userID)
 	if orderRegErr != nil {
@@ -122,15 +135,68 @@ func (uh URLHandler) UserGetOrders(w http.ResponseWriter, r *http.Request) {
 
 // UserGetBalance process GET /api/user/balance request
 func (uh URLHandler) UserGetBalance(w http.ResponseWriter, r *http.Request) {
-	//
+	logger.Info("UserGetBalance hit by GET /api/user/balance")
+	userID := auth.GetUserID(r.Context())
+
+	balance, err := uh.store.GetBalance(r.Context(), userID)
+	// TODO divide currency ints by 100
+	jr, err := json.Marshal(balance)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jr)
 }
 
 // UserBalanceWithdraw process POST /api/user/balance/withdraw request
 func (uh URLHandler) UserBalanceWithdraw(w http.ResponseWriter, r *http.Request) {
-	//
+	logger.Info("UserBalanceWithdraw hit by POST /api/user/balance/withdraw")
+	userID := auth.GetUserID(r.Context())
+
+	req, err := models.ReadWithdrawRequestFromBody(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if !req.Validate() {
+		http.Error(w, "wrong number format", http.StatusUnprocessableEntity)
+		return
+	}
+
+	err = uh.store.ProcessWithdraw(r.Context(), req, userID)
+	if err != nil {
+		if errors.Is(err, storage.ErrInsufficientFunds) {
+			http.Error(w, err.Error(), http.StatusPaymentRequired)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// http.StatusOK sent implicitly
 }
 
 // UserGetWithdrawals process GET /api/user/balance/withdrawals request
 func (uh URLHandler) UserGetWithdrawals(w http.ResponseWriter, r *http.Request) {
-	//
+	logger.Info("UserGetWithdrawals hit by GET /api/user/balance/withdrawals")
+	userID := auth.GetUserID(r.Context())
+	withdrawals, err := uh.store.GetWithdrawals(r.Context(), userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if len(withdrawals) == 0 {
+		http.Error(w, "no withdrawals", http.StatusNoContent)
+		return
+	}
+	jr, err := json.Marshal(withdrawals)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jr)
 }
--- api/handlers/handlers_test.go
+++ api/handlers/handlers_test.go
@@ -4,14 +4,17 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/sbxb/loyalty/api/handlers"
+	mw "github.com/sbxb/loyalty/api/middleware"
 	"github.com/sbxb/loyalty/config"
 	"github.com/sbxb/loyalty/models"
 	"github.com/sbxb/loyalty/storage/inmemory"
@@ -215,9 +218,66 @@ func TestUserLogin_ValidInput(t *testing.T) {
 	}
 }
 
+func TestUserPostOrder_ValidInput(t *testing.T) {
+	tests := []struct {
+		wantCode    int
+		orderNumber string
+	}{
+		{
+			wantCode:    202,
+			orderNumber: "12345678903",
+		},
+		{
+			wantCode:    200,
+			orderNumber: "12345678903",
+		},
+	}
+
+	store, _ := inmemory.NewMapStorage() // NewMapStorage() never returns non-nil error
+	router := chi.NewRouter()
+	urlHandler := handlers.NewURLHandler(store, cfg)
+	router.With(mw.AuthMW).Post("/api/user/orders", urlHandler.UserPostOrder)
+
+	// add the first user
+	user := &models.User{
+		Login: "user",
+		Hash:  "$2a$10$2V0TfI3A/Win8OI5Q.U1gOjffxfBxX9bLUa7Zheo3jKOaxAzwEDYa",
+	}
+	err := store.AddUser(context.Background(), user)
+	require.NoError(t, err)
+
+	for _, tt := range tests {
+		t.Run("Add Order", func(t *testing.T) {
+			req := httptest.NewRequest(
+				http.MethodPost,
+				"http://"+cfg.ServerAddress+"/api/user/orders",
+				strings.NewReader(tt.orderNumber),
+			)
+			cookie := http.Cookie{
+				Name:    "user",
+				Value:   "47dd2e0ab2fc35c8bd9d56847a904a3ca3cf166c5c1ffb1b4f8a87337b134ebcNoWIWEqALp/7+sHKF7Dq5/mpSvupLXgkpj6TDcdXeu/almqW9dykJ6hVZQGx/nPnhlFv",
+				Expires: time.Now().Add(1 * time.Hour),
+			}
+			req.AddCookie(&cookie)
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, req)
+
+			resp := w.Result()
+			defer resp.Body.Close()
+
+			assert.Equal(t, resp.StatusCode, tt.wantCode)
+			t.Log(resp.StatusCode)
+			store.DumpOrder()
+			store.DumpBalance()
+		})
+	}
+
+}
+
 func checkCookie(resp *http.Response, key string) bool {
 	for _, c := range resp.Cookies() {
 		if c.Name == key {
+			fmt.Println(c.Value)
 			return true
 		}
 	}
